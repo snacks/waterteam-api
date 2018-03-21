97f1099a350fe75b3d34af283f6951d86d155dbf
AFlowOfCode <aflowofcode@gmail.com>
2018-03-21 11:35:54 -0600

    failed login stays on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+
   devise_scope :user do
     get "/register" => "authreg#new", as: "new_user_registration" # custom path to sign_up/registration
   end
 
   devise_for :users
-  # devise_for :users, :controllers => { :registrations => 'authorizedregistration' }
+  # devise_scope :user do
+  #   authenticated :user do
+  #     root 'home#index', as: :authenticated_root
+  #   end
+
+  #   unauthenticated do
+  #     root 'home#index', as: :unauthenticated_root
+  #   end
+  # end
   resources :notes
   resources :deployments
   resources :timeseries
@@ -12,5 +22,4 @@ Rails.application.routes.draw do
   resources :devices
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
 end
