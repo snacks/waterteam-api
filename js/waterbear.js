73c4bfa1d55abbb645ef365e83866ac5bff0a454
snacks <gwen@fromthegut.org>
2018-10-10 16:42:00 -0700

    moved js into its own file and added some preliminary structure for adding points to the map

---
+++ js/waterbear.js
@@ -0,0 +1,59 @@
+$(document).ready(function() {
+
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoic3NzbmFja3MiLCJhIjoiY2ptanQxMHZ6MDI0dzNrbGNlODg4NXprYiJ9.8Fl_Mzmv_jSApsyVW_lyfg';
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/sssnacks/cjn24qmxk6hax2rn62sjbysj7',
+  center: [-112.942500, 37.268600],
+  zoom: 14.0
+});
+
+map.on('load', function () {
+map.addSource('firstSonde', {
+  type: 'geojson',
+  data: 'firstSonde.geojson'
+  });
+
+map.addLayer({
+	"id": "firstSonde",
+	"type": "circle",
+	"source": "firstSonde",
+	"paint": {
+	"circle-radius": 5,
+	"circle-color": "#ff0000"
+	}
+	});
+});
+
+//zoom in double click, zoom out shift double click
+map.addControl(new mapboxgl.NavigationControl());
+map.scrollZoom.disable();
+
+// Add geolocate control to the map.
+map.addControl(new mapboxgl.GeolocateControl({
+    positionOptions: {
+        enableHighAccuracy: true
+    },
+    trackUserLocation: true
+}));
+
+(function($) {
+window.fnames = new Array();
+window.ftypes = new Array();
+fnames[0]='EMAIL';
+ftypes[0]='email';
+fnames[1]='FNAME';
+ftypes[1]='text';
+fnames[2]='LNAME';
+ftypes[2]='text';
+fnames[3]='ADDRESS';
+ftypes[3]='address';
+fnames[4]='PHONE';
+ftypes[4]='phone';
+fnames[5]='BIRTHDAY';
+ftypes[5]='birthday';}
+(jQuery));
+var $mcj = jQuery.noConflict(true);
+
+});
